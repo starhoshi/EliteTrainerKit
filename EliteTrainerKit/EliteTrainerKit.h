8e7fa6910988899776ab4cf926a916b47bba8be1
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2018-08-22 21:35:13 +0900

    initial commit

---
+++ EliteTrainerKit/EliteTrainerKit.h
@@ -0,0 +1,19 @@
+//
+//  EliteTrainerKit.h
+//  EliteTrainerKit
+//
+//  Created by kensuke-hoshikawa on 2018/08/22.
+//  Copyright © 2018年 star__hoshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EliteTrainerKit.
+FOUNDATION_EXPORT double EliteTrainerKitVersionNumber;
+
+//! Project version string for EliteTrainerKit.
+FOUNDATION_EXPORT const unsigned char EliteTrainerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EliteTrainerKit/PublicHeader.h>
+
+
